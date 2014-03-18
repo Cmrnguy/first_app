67532f351b55ec80b0e01e6f6547e4618b8afc4b
camerondguy@gmail.com <camg@Cams-MacBook-Pro.local>
2014-03-17 23:04:20 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'e2010675b8bca5b0d2c8691030520c8431f7329909e4adaf5f00aad0a1ff8924676a7d7103168f7f7e4fc5cbc4a85c1e7e6a4614b3b27188a8e085edebadf335'
